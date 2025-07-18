0ddf00e3d28748f94651049494f832a13f55b527
Jorell Relleve <jorellrelleve@gmail.com>
2025-07-18 23:00:44 +0800

    add file text vview in upload file page

--- client/src/pages/SendFile.tsx
+++ client/src/pages/SendFile.tsx
@@ -6,15 +6,17 @@ import Button from '../components/Button';
 const SendFile = () => {
   const [fileName, setName] = useState<string | null>(null);
   const [file, setFile] = useState<string | null>(null);
+  const [text, setText] = useState<string | null>(null);
   
   const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setFile(prevFile => e.target.files[0]);
-    setName(prevName => e.target.files[0].name);
+    setName(prevName => e.target.files[0]?.name);
   }, []);
   
   const removeFile = useCallback(() => {
     setFile(prevFile => null);
     setName(prevFile => null);
+    setText(prevText => null);
   }, []);
   
   const handleSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
@@ -31,7 +33,7 @@ const SendFile = () => {
     });
 
     const data = await result.json();
-    console.log(data);
+    setText(data.fileText);
     } catch (err) {
       console.error(err);
     }
@@ -40,13 +42,16 @@ const SendFile = () => {
   return (
     <div className="w-full min-h-screen bg-gradient-to-b from-zinc-950 via-zinc-950 to-emerald-950 flex md:flex-row flex-col justify-center items-center gap-y-3 p-5"> 
        <form name="sendFile" className="w-full md:w-[25rem] flex flex-col gap-y-3" onSubmit={handleSubmit}>
-         <label htmlFor="fileSender" className="p-2 rounded-md border border-zinc-800 text-zinc-200 w-full text-center">Upload File</label>
+         <label htmlFor="fileSender" className="p-2 rounded-md border border-zinc-800 text-zinc-200 w-full text-center bg-zinc-900">Upload File</label>
          {fileName && <div className="w-full flex gap-x-2">
            <span className="p-2 rounded-md border border-dashed border-zinc-800 text-zinc-200 w-full text-center">{fileName}</span>
            <Button className="p-2 text-emerald-50 bg-emerald-600 rounded-md active:bg-emerald-700" icon={<X size={22} />} onClick={removeFile} />
          </div>}
          <input type="file" id="fileSender" accept=".doc,.docx,.pdf" onChange={handleChange} className="hidden"/>
          <Button className={`p-2 bg-emerald-600 text-emerald-50 rounded-md font-medium ${!file ? "bg-emerald-800 text-emerald-300" : "active:bg-emerald-700"}`}type="submit" text="Submit File" />
+         {text && <div className="w-full rounded-md border border-zinc-800 bg-zinc-900 p-2">
+            <span className="text-zinc-200">{text}</span>
+         </div>}
        </form>
     </div>
     );
--- server/src/controllers/uploadController.ts
+++ server/src/controllers/uploadController.ts
@@ -13,9 +13,11 @@ const uploadController = async (req: Request, res: Response, next: NextFunction)
     if(extention === "docx" || extention === "doc"){
       const docxText = await extractTextFromDocs(buffer ?? null)
       console.log("Doc text: ", docxText)
+      res.status(200).json({ fileText: docxText });
     }else if(extention === "pdf"){
       const pdfText = await extractTextFromPdf(buffer ?? null)
       console.log("Pdf text", pdfText);
+      res.status(200).json({ fileText: pdfText });
     }
   }catch(err){
     next(err)
